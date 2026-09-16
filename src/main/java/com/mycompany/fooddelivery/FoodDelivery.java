33707d7192ba4dd50f269512625b5bd62845c936
christian dolotallas <dolotallaschristian2@gmail.com>
2026-09-16 09:42:56 +0800

    Stating Basis

---
+++ src/main/java/com/mycompany/fooddelivery/FoodDelivery.java
@@ -0,0 +1,10 @@
+package com.mycompany.fooddelivery;
+
+public class FoodDelivery {
+
+    public static void main(String[] args) {
+       
+        fooddeliveryGUI fooddelivery = new fooddeliveryGUI();
+        fooddelivery.setVisible (true);
+    }
+}
---
+++ src/main/java/com/mycompany/fooddelivery/fooddeliveryGUI.java
@@ -0,0 +1,14 @@
+package com.mycompany.fooddelivery;
+import java.util.*;
+import javax.swing.*;
+public class fooddeliveryGUI extends JFrame {
+    
+    private JLabel lblheader;
+    
+    fooddeliveryGUI(){
+        setSize(600, 700);
+        setLayout(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+    }
+}
