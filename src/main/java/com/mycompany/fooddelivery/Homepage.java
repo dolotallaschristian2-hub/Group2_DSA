0cfcf4a0cfe700421b958eb59acf4a04977e0ce5
Paul George Padil <paulgeorgepadil0@gmail.com>
2026-09-24 11:54:57 +0800

    Sisimula palang

---
+++ src/main/java/com/mycompany/fooddelivery/Homepage.java
@@ -0,0 +1,20 @@
+package com.mycompany.fooddelivery;
+
+import javax.swing.*;
+
+public class Homepage extends JFrame  {
+    
+    private JLabel lblDashboard, lblWelcome;
+    private JMenu menu;
+    private JButton profile, settings, logout;
+   
+            
+    Homepage(){
+        setSize(600, 600);
+        setLayout(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+    }
+    
+}
